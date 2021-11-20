66de90f0ba0da7012d2e0ea4aef2b9f0e9107ab3
royezclap <88072856+royezclap@users.noreply.github.com>
2021-11-20 22:39:33 +1300

    Add files via upload

---
+++ loolo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Datatype int: %d bytes.\n", sizeof(int));
+    printf("Datatype char: %d bytes.\n", sizeof(char));
+    printf("Datatype long: %i bytes.\n", sizeof(long));
+    printf("Datatype double: %d bytes.\n", sizeof(double));
+}
