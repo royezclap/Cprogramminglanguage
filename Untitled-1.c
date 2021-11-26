0d176d2545b6749f006bb65f4c7265ac4099eb80
royezclap <88072856+royezclap@users.noreply.github.com>
2021-11-26 19:56:05 +1300

    Add files via upload

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,31 +1,17 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    float f1 = 123.456, f2;
-    int i1, i2 = -150 ;
-
-    i1 = f1; //floating to interger conversion
-    printf("%f assigned to an in produces %i \n", f1, i1);
-    
-    f1 = i2; //interger to floating conversion
-    printf("%i assigned to an in produces %f \n", i1, f1);
-
-    f1 = i2 / 100; //interger divided by interger
-    printf("%i divided by 100 produces %f \n", i2, f1);
-    
-    f2 = i2 / 100.0; //interger divided by a float
-    printf("%i divided by 100.0 produces %f \n", i2, f2); 
-
-    f2 = (float) i2 / 100; //type cast operator
-    printf("(float) %i divided by 100 produces %f \n", i2, f2);
-
+    float x1 = 1.0;
+    float y1 = 2.0;
+    float x2 = 3.0;
+    float y2 = 3.0;
+    float x3 = -2.0;
+    float y3 = 2.0;
+    printf("pow(y1, 6) + x1 * pow(y1, 5) + pow(x1, 2) * pow(y1, 4) + pow(x1, 3) * pow(y1, 3) + pow(x1, 4) * pow(y1, 2) + pow(x1, 5) * y1 + pow(x1, 6) = %f\n", pow(y1, 6) + x1 * pow(y1, 5) + pow(x1, 2) * pow(y1, 4) + pow(x1, 3) * pow(y1, 3) + pow(x1, 4) * pow(y1, 2) + pow(x1, 5) * y1 + pow(x1, 6));
+    printf("pow(y2, 6) + x2 * pow(y2, 5) + pow(x2, 2) * pow(y2, 4) + pow(x2, 3) * pow(y2, 3) + pow(x2, 4) * pow(y2, 2) + pow(x2, 5) * y2 + pow(x2, 6) = %f\n", pow(y2, 6) + x2 * pow(y2, 5) + pow(x2, 2) * pow(y2, 4) + pow(x2, 3) * pow(y2, 3) + pow(x2, 4) * pow(y2, 2) + pow(x2, 5) * y2 + pow(x2, 6));
+    printf("pow(y3, 6) + x3 * pow(y3, 5) + pow(x3, 2) * pow(y3, 4) + pow(x3, 3) * pow(y3, 3) + pow(x3, 4) * pow(y3, 2) + pow(x3, 5) * y3 + pow(x3, 6) = %f\n", pow(y3, 6) + x3 * pow(y3, 5) + pow(x3, 2) * pow(y3, 4) + pow(x3, 3) * pow(y3, 3) + pow(x3, 4) * pow(y3, 2) + pow(x3, 5) * y3 + pow(x3, 6));
     return 0;
+
 }
-
-//123.456001 assigned to an in produces 123 
-//123 assigned to an in produces -150.000000
-//-150 divided by 100 produces -1.000000
-//-150 divided by 100.0 produces -1.500000
-//(float) -150 divided by 100 produces -1.500000
-
---
+++ hi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float x1 = 1.0;
+    float y1 = 2.0;
+    float x2 = 3.0;
+    float y2 = 3.0;
+    float x3 = -2.0;
+    float y3 = 2.0;
+    float result;
+    result = pow(y1, 6) + x1 * pow(y1, 5) + pow(x1, 2) * pow(y1, 4) + pow(x1, 3) * pow(y1, 3) + pow(x1, 4) * pow(y1, 2) + pow(x1, 5) * y1 + pow(x1, 6);
+    printf("pow(y1, 6) + x1 * pow(y1, 5) + pow(x1, 2) * pow(y1, 4) + pow(x1, 3) * pow(y1, 3) + pow(x1, 4) * pow(y1, 2) + pow(x1, 5) * y1 + pow(x1, 6) = %f\n", result);
+    result = pow(y2, 6) + x2 * pow(y2, 5) + pow(x2, 2) * pow(y2, 4) + pow(x2, 3) * pow(y2, 3) + pow(x2, 4) * pow(y2, 2) + pow(x2, 5) * y2 + pow(x2, 6);
+    printf("pow(y2, 6) + x2 * pow(y2, 5) + pow(x2, 2) * pow(y2, 4) + pow(x2, 3) * pow(y2, 3) + pow(x2, 4) * pow(y2, 2) + pow(x2, 5) * y2 + pow(x2, 6) = %f\n", result);
+    result = pow(y3, 6) + x3 * pow(y3, 5) + pow(x3, 2) * pow(y3, 4) + pow(x3, 3) * pow(y3, 3) + pow(x3, 4) * pow(y3, 2) + pow(x3, 5) * y3 + pow(x3, 6);
+    printf("pow(y3, 6) + x3 * pow(y3, 5) + pow(x3, 2) * pow(y3, 4) + pow(x3, 3) * pow(y3, 3) + pow(x3, 4) * pow(y3, 2) + pow(x3, 5) * y3 + pow(x3, 6) = %f\n", result);
+    return 0;
+
+}
