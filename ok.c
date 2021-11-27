b143c1339b63aaacfd5ade2726b1026b70f07ef5
royezclap <88072856+royezclap@users.noreply.github.com>
2021-11-27 14:13:19 +1300

    Add files via upload

---
+++ ok.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a = 43; //Ba
+    int b = 40; //Mẹ
+    int c = 13; //Hiển
+    int d = 8; //Em trai
+    printf("a + b + c + d = %i\n", a + b + c + d);
+    printf("a * b * c * d = %i\n", a * b * c * d);
+    printf("(a + b + c + d) / 4 = %i\n", (a + b + c + d) / 4);
+    return 0;
+}
