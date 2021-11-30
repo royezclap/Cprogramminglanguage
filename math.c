d13bea67ffafe349487a9f56cb222acfa057dad5
royezclap <88072856+royezclap@users.noreply.github.com>
2021-11-30 19:21:54 +1300

    Add files via upload

---
+++ math.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+    float a, b, P, S;
+    a > 0;
+    b > 0;
+    printf("Enter a, b: ");
+    scanf("%f, %f", &a, &b);
+    P = a*2+b*2;
+    S = a * b;
+    printf("if a=%f b=%f then P=%f S=%f\n", a, b, P, S);
+    printf("The Rectangle: perimeter=%f, area=%f", P, S);
+
+
+}
