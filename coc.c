11ead12b2f9e691ef5d3afc1f68179d6ae9936db
royezclap <88072856+royezclap@users.noreply.github.com>
2021-12-04 15:50:05 +1300

    coc commit

---
+++ coc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    char a, b;
+    printf("Enter character: ");
+    scanf("%c", &a);
+    printf("%d\n", a);
+    printf("Enter number:  ");
+    scanf("%i", &b);
+    printf("%c", b);
+}
