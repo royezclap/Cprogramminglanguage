72787591c0d1389c1e2066f69a2a8e03f3275243
royezclap <88072856+royezclap@users.noreply.github.com>
2021-12-10 21:14:51 +1300

    Add files via upload

--- sup.c
+++ sup.c
@@ -3,11 +3,26 @@
 
 int main()
 {
-    float a,b,c,d,e,x,y;
-    printf("Nhập vào điểm toán, lý, hóa, văn và tiếng anh của học sinh:  ");
-    scanf("%f,%f,%f,%f,%f", &a,&b,&c,&d,&e );
-    x=a+b+c+d+e;
-    y=x/5;
-    printf("if a=%f b=%f c=%f d=%f e=%f then x=%f y=%f", a,b,c,d,e,x,y);
+    int n;
+    printf("Age: ");
+    scanf("%i", &n);
+    if(n<6){
+        printf("Tuoi thieu nhi");
+    }
+    else if(6<n&&n<12){
+        printf("Tuoi thieu nien");
+    }
+    else if(12<n&&n<19){
+        printf("Tuoi thanh thieu nien");
+    }
+    else if(19<n&&n<35){
+        printf("Tuoi thanh nien");
+    }
+    else if(35<n&&n<60){
+        printf("Tuoi trung nien");
+    }
+    else if(60<n){
+        printf("Tuoi cao nien");
+    }
     
 }
