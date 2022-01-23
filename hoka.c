428e910057000d56b2b7a4028ce63b1e23b7bf57
royezclap <88072856+royezclap@users.noreply.github.com>
2022-01-23 16:34:19 +1300

    Add files via upload

---
+++ hoka.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char s[10];
+    char s1[10];
+    int ok = 1;
+
+    printf("enter string: ");
+    gets(s);
+
+    printf("enter string1: ");
+    gets(s1);
+
+    int size_s1 = strlen(s1);
+
+    for (int i = 0; i < size_s1; i++)
+    {
+        if (s[i] != s1[i])
+        {
+            ok = 0;
+            break;
+        }
+    }
+
+    if (ok == 1)
+    {
+        printf("string1 is the start of string ");
+    }
+    else
+        printf("string1 is not the start of string");
+}
