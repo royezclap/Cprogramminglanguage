4d6284cc02d9d3525dfeca136c5b9e10fdb8e2e9
royezclap <88072856+royezclap@users.noreply.github.com>
2022-01-26 20:50:24 +1300

    Add files via upload

---
+++ ham5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int capChungThanThiet(int M, int N)
+{
+    int flag = 0, flag2 = 0;
+    int sum = 0;
+    for (int i = 1; i <= M; ++i)
+    {
+        if (M % i == 0)
+        {
+            sum+=i;
+            if(sum == N)
+                flag = 1;
+        }
+    }
+
+    sum = 0;
+    
+    for (int i = 1; i <= N; ++i)
+    {
+        if (N % i == 0)
+        {
+            sum+=i;
+            if(sum == M)
+                flag2 = 1;
+        }  
+
+    }
+
+    if(flag == 1 && flag2 == 1)
+    {
+        printf("%d va %d la cap so than thiet!", N, M);
+    }
+    else printf("error");
+}
+
+int main()
+{
+    int M, N;
+    capChungThanThiet(284, 220);
+}
