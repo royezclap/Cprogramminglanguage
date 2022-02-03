81045bc2e8b4e5b8b1a2427de67c61b0a76f37bf
royezclap <88072856+royezclap@users.noreply.github.com>
2022-02-03 17:27:05 +1300

    Add files via upload

---
+++ ham10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int perfect(int n)
+{
+    int sum, tong = 0;
+    int flag = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = 0;
+
+        for (int j = 1; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                sum += j;
+            }
+        }
+
+        if (sum == i)
+        {
+            printf("%d, ", i);
+            tong += i;
+            printf("%d", tong);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    do
+    {
+        printf("enter n: ");
+        scanf("%d", &n);
+    } while (n < 0 && n > 1000);
+
+    perfect(n);
+}
