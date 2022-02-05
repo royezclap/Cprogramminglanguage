8c50e69e47bc11d9540a9c966020db6bc860b123
royezclap <88072856+royezclap@users.noreply.github.com>
2022-02-05 17:55:27 +1300

    Add files via upload

--- capsothanthiet.c
+++ capsothanthiet.c
@@ -8,32 +8,31 @@ int capChungThanThiet(int M, int N)
     {
         if (M % i == 0)
         {
-            sum+=i;
+            sum += i;
         }
-
-        if(sum == N)
-            flag = 1;
     }
+    if (sum == N)
+        flag = 1;
 
     sum = 0;
-    
+
     for (int i = 1; i < N; ++i)
     {
         if (N % i == 0)
         {
-            sum+=i;
-        }  
-
-        if(sum == M)
-            flag2 = 1;
-
+            sum += i;
+        }
     }
 
-    if(flag == 1 && flag2 == 1)
+    if (sum == M)
+        flag2 = 1;
+
+    if (flag == 1 && flag2 == 1)
     {
         printf("%d va %d la cap so than thiet!", N, M);
     }
-    else printf("error");
+    else
+        printf("error");
 }
 
 int main()
@@ -47,6 +46,6 @@ int main()
         printf("enter m: ");
         scanf("%d", &M);
 
-    }while (N > 2000 && M < 1);
+    } while (N > 2000 && M < 1);
     capChungThanThiet(M, N);
 }
