c7f0188cc69c193df53986491e137e2bc0d5442d
royezclap <88072856+royezclap@users.noreply.github.com>
2022-02-05 17:23:36 +1300

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int is_prime(int n)
+{
+
+    int count = 0;
+    int sum = 0;
+    int j;
+    
+    for (int i = 2; i < n; i++)
+    {
+        int c = 0;
+        for (j = 1; j <= i; j++)
+        {
+            if (i % j == 0)
+            {
+                c++;
+            }
+        }
+
+        if (c == 2)
+        {
+            sum+=i;
+            printf("Prime Number: %d\n", i);
+        }
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int n;
+    do
+    {
+        printf("enter n: ");
+        scanf("%d", &n);
+    } while (n < 0 && n > 200);
+
+    int c = prime(n);
+    printf("%d", c);
+}
