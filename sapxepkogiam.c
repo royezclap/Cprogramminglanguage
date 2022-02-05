5f9e7d18e5f462a75c3f1e2bf327b224a726edc9
royezclap <88072856+royezclap@users.noreply.github.com>
2022-02-05 16:13:35 +1300

    Add files via upload

---
+++ sapxepkogiam.c
@@ -0,0 +1,37 @@
+    #include <stdio.h>
+
+int is_sort(int A[], int n)
+{
+    int d = 1, i = 0;
+
+    while ((d == 1) && i < n - 1)
+    {
+        if (A[i] < A[i+1])
+            d = 0;
+        i++;
+    }
+
+    if (d == 1)
+    {
+        return 0;
+    }
+    else return 1;
+}
+int main()
+{
+    int n, A[15];
+
+    printf("enter number of numbers present in array pls: ");
+    scanf("%d", &n);
+
+    printf("Enter array pls: ");
+
+    for (int i = 0; i < n; i++)
+        scanf("%d", &A[i]);
+
+    
+    printf("\n");
+
+    int c = is_sort(A, n);
+    printf("%d", c);
+}
