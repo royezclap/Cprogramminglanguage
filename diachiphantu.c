dd42a1503e037ce94d59efdf9768b891b3f95f99
royezclap <88072856+royezclap@users.noreply.github.com>
2022-02-25 18:03:48 +1300

    Add files via upload

---
+++ diachiphantu.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[15];
+    int n;
+
+    do
+    {
+        printf("Enter n: ");
+        scanf("%d", &n);
+    } while (n > 10);
+
+    printf("Enter array: ");
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d ", &a[i]);
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n");
+    
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d ", &a[i]);
+    }
+    
+}
