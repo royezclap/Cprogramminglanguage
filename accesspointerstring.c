1792b5cb282671cc4d24061df0b6e3cea3e5ef45
royezclap <88072856+royezclap@users.noreply.github.com>
2022-03-08 17:30:01 +1300

    Add files via upload

---
+++ accesspointerstring.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char *days[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    printf("%s", days[0]);
+    printf("\n");
+    printf("%s", days[1]);
+    printf("\n");
+    printf("%s", days[2]);
+    printf("\n");
+    printf("%s", days[3]);
+    printf("\n");
+    printf("%s", days[4]);
+    printf("\n");
+    printf("%s", days[5]);
+    printf("\n");
+    printf("%s", days[6]);
+    printf("\n");
+    printf("%s", days[7]);
+
+}
